c3855f44720e0c62aee44df52c19d0848f546167
Rafael Sousa <43481051+RafaaSousa@users.noreply.github.com>
2024-04-15 15:34:20 -0300

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
 function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
+    photo.src = profileData.information.photo
+    photo.alt = profileData.information.name
 
     const name = document.getElementById('profile.name')
-    name.innerText = profileData.name
+    name.innerText = profileData.information.name
 
     const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
+    job.innerText = profileData.information.job
 
     const location = document.getElementById('profile.location')
-    location.innerText = profileData.location
+    location.innerText = profileData.information.location
 
     const phone = document.getElementById('profile.phone')
-    phone.innerText = profileData.phone
-    phone.href = `tel.:${profileData.phone}`
+    phone.innerText = profileData.information.phone
+    phone.href = `tel.:${profileData.information.phone}`
 
     const email = document.getElementById('profile.email')
-    email.innerText = profileData.email
+    email.innerText = profileData.information.email
     email.href = `mailto:${profileData.email}`
 
     const github = document.getElementById('profile.github')
-    github.innerText = profileData.github
-    github.href = `https://github.com/{$profileData.github}`
+    github.innerText = profileData.information.github
+    github.href = `https://github.com/${profileData.information.github}`
 
 }
 
